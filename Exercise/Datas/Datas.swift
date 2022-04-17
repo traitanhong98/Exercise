665bc050975345194d774cd1ca4612bf30533f2f
An <annx2812@gmail.com>
2022-04-17 21:35:31 +0700

    - Add files

---
+++ Exercise/Datas/Datas.swift
@@ -0,0 +1,8 @@
+//
+//  Datas.swift
+//  Exercise
+//
+//  Created by Nguyễn Hoàng on 17/04/2022.
+//
+
+import Foundation
---
+++ Exercise/Models/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Exercise
+//
+//  Created by Nguyễn Hoàng on 17/04/2022.
+//
+
+import Foundation
