c0eb7d6da3bf45ded4558ecb7623af5de02f7b40
ECO0813-THANGNV <thangnguyen4@tdt.asia>
2022-04-19 09:19:10 +0700

    add  scene main

---
+++ Exercise/Controllers/Cell/EmployeeTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  EmployeeTableViewCell.swift
+//  Exercise
+//
+//  Created by ECO0813-THANGNV on 4/18/22.
+//
+
+import UIKit
+
+class EmployeeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var showName: UILabel!
+    @IBOutlet weak var showAddress: UILabel!
+    @IBOutlet weak var showSex: UILabel!
+    @IBOutlet weak var showPosition: UILabel!
+    
+    @IBAction func btn_edit(_ sender: Any) {
+        
+    }
+    @IBAction func btn_delete(_ sender: Any) {
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+     
+    }
+    
+ 
+    
+    
+
+    
+    
+
+    
+    
+}
